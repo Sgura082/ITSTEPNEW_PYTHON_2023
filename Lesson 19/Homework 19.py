c7e5636ff617bda1042f478b03ee3dd9d31f889f
Saba gurashvili <Sgura082@gmail.com>
2024-02-22 23:19:36 +0400

    Added descritptions and comments to Stack code

--- Lesson 19/Homework 19.py
+++ Lesson 19/Homework 19.py
@@ -40,8 +40,8 @@ class Task():
 # -----------CUSTOM FUNCTIONS----------------
 
 # -----------TASK OBJECTS----------------
-Task01 = Task("Describing code")
-Task02 = Task("x")
+Task01 = Task("Describing LinkedList code")
+Task02 = Task("Describing Stack code")
 Task03 = Task("x")
 
 
@@ -80,17 +80,20 @@ def task1_body():
             """
             new_node = Node(data)  # Creates a new Node class object which stores argument ('data')
 
-            if self.head is None:  # Checks whether the list is empty or not by checking the 'head'
-                                    # variable (which equals None by default). If its empty then the new_node
-                                    # object is stored as the 'head' (first element of LinkedList)
+            if self.head is None:
+                # Checks whether the list is empty or not by checking the 'head'
+                # variable (which equals None by default). If its empty then the new_node
+                # object is stored as the 'head' (first element of LinkedList)
                 self.head = new_node
                 return
 
-            last_node = self.head #In order to add the new_node as the last element/node system needs to identify
+            last_node = self.head
+            #In order to add the new_node as the last element/node system needs to identify
             # the current last Node within the LinkedList. To do so every Node will be checked whether it is last or not.
             # So system starts with the first node of list.
 
-            while last_node.next: #In order for the Node within the LinkedList to be the last one
+            while last_node.next:
+                #In order for the Node within the LinkedList to be the last one
                 # the Node.next == None must be true, since this variable stores data about the node after
                 # it in the linked list. This while loop works while the 'next' is filled, constantly appointing
                 # the Node after the current one until there is no next Node. As a result of loop the current las node is
@@ -110,7 +113,8 @@ def task1_body():
             """
             new_node = Node(data) # Creates a new Node class object which stores argument ('data')
 
-            if index == 0: #If the desired index is 0 then current head of the Linked list is appointed as 'next' for
+            if index == 0:
+                #If the desired index is 0 then current head of the Linked list is appointed as 'next' for
                 # new_node since it becomes the head(first node) of the LinkedList
                 new_node.next = self.head
                 self.head = new_node
@@ -119,7 +123,8 @@ def task1_body():
             current_node = self.head
             current_index = 0
 
-            while current_node.next and current_index < index - 1: #In order to find the Node in list that comes
+            while current_node.next and current_index < index - 1:
+                # In order to find the Node in list that comes
                 # before the desired index system checks every Node from the very start. While the checked(current) Node
                 # has 'next' filled and its index is below the index of a Node that comes before the insertion
                 # index is incremented and next nodes are set as current ones
@@ -156,16 +161,16 @@ def task1_body():
                 # The value of 'next' for current node is set as the removable nodes next Node(node that came after the removable node)
                 current_node.next = current_node.next.next
 
-        def display_info(self): # prints the contents of the Linked list
+        def display_info(self):
+            """
+            prints the contents of the Linked list by printing Node.data for every node starting from head.
+            :return:
+            """
             current_node = self.head
-
             while current_node is not None:
                 print(current_node.data, end=" -> ")
                 current_node = current_node.next
             print()
-
-    a = LinkedList()
-    a.insert()
     linked_list = LinkedList()
     linked_list.append(10)
     linked_list.append(2)
@@ -188,17 +193,102 @@ Task01.write_function(task1_body)
 
 
 def task2_body():
-    pass
+    class Node:
+        """
+        Class for creation of Node objects, the elements of Stack
+        """
+        def __init__(self, data):
+            """
+            Initiates the object of Node Class by saving given argument in 'data'. It also keeps information about
+            the Node that comes after current one in the LinkedList in 'next'
+            """
+            self.data = data
+            self.next = None
 
+    class Stack:
+        """
+        Class for creation of Stack objects
+        """
+        def __init__(self):
+            """
+            Initiates the object of Stack Class.
+            """
+            self.top_node = None
+            self.length = 0
+
+        def empty(self):
+            """
+            Checks whether the stack is empty or not by returning True or False whether length is 0=(True) or >0=(False)
+            :return:
+            """
+            return self.length == 0
+
+        def size(self):
+            """
+            gets the length of the stack
+            :return:
+            """
+            return self.length
+
+        def top(self):
+            """
+            gets the top node in the stack
+            :return:
+            """
+            if not self.empty(): #if the list isn't empty returns the top node
+                                    # else raises an IndexError notifying that stack is empty
+                return self.top_node.data
+            else:
+                raise IndexError("Stack Is Empty")
+
+        def push(self, data):
+            """
+            adds the inserted parameter 'data' as the first node of the stack, pushing the original nodes in the
+            sequence by one.
+            :param data: 'data' to be added to the stack as first element
+            :return:
+            """
+            new_node = Node(data)
+
+            new_node.next = self.top_node # Current fist node is set as the next node for new_node making
+                                            # it the second node in stack
+            self.top_node = new_node #new_node is set as the first element of stack
+            self.length += 1 # lenght of stack increased by 1
+
+        def pop(self):
+            """
+            Removes the first element(top_node) of the stack
+            :return: returns data of the popped element
+            """
+            if not self.empty():
+                popped_item = self.top_node.data #saves data of poped node in order to return it
+                self.top_node = self.top_node.next #sets the second node of the stack as the top_node(first element) of stack
+                self.length -= 1 # reduces the length of stack
+                return popped_item #returns the data of the popped node
+            else:
+                raise IndexError("Stack Is Empty")
+
+    stack = Stack()
+    stack.push(1)
+    stack.push(5)
+    stack.push(2)
+    stack.push(8)
+    stack.push(10)
+    print(stack.size())
+    print(stack.pop())
+    print(stack.size())
+    print(stack.pop())
+    print(stack.size())
+    print(stack.pop())
+    print(stack.size())
+    print(stack.pop())
+    print(stack.size())
+    print(stack.pop())
 
 Task02.write_function(task2_body)
 
 
-def task3_body():
-    pass
 
-
-Task03.write_function(task3_body)
 
 # -------------MAIN CODE---------------------------------------------------------------------
 
