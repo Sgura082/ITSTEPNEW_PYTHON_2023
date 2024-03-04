c8a9fb3613b0ad2c664ac05ac1690db52b77dd8d
Saba gurashvili <Sgura082@gmail.com>
2024-03-04 23:55:06 +0400

    last commit

--- Lesson22/Homework22.py
+++ Lesson22/Homework22.py
@@ -1,5 +1,6 @@
-import re
-import pickle
+import json
+import copy
+
 # --------------------TASK DESCRIPTIONS-------------------------------
 # ------Task1: Serialization and Deserialization using modules
 # ------Task2:
@@ -40,39 +41,70 @@ class Task():
 # -----------CUSTOM FUNCTIONS----------------
 
 # -----------TASK OBJECTS----------------
-Task01 = Task("x")
-Task02 = Task("x")
-Task03 = Task("x")
-
-
+Task01 = Task("Serialization and Deserialization using modules")
 def task1_body():
     pass
+
     # ----------------Task Variables----------------------------------
+
     # ----------------Task Classes------------------------------------
+    class Movie():
+        def __init__(self, dict):
+            for key, value in dict.items():
+                self.__dict__[key] = value
+        def __str__(self):
+            return f"{vars(self)}"
+
     # ----------------Task Functions----------------------------------
+    def cus_encoder(obj):
+        """
+        Translates Movie class objects in data into dictionaries
+        :param obj:
+        :return:
+        """
+        for i in obj:
+            for movie in i['results']:
+                if isinstance(movie, Movie):
+                    index = i['results'].index(movie)
+                    i['results'][index] = vars(movie)
+        return obj
+
+
+    def seek_create_modify(json_data):
+        """
+        Checks if a movie complies with predefined conditions, if yes then new instance (deepcopy) of it
+        is created with the genres modified and  the list of movies is appended by this new instance
+        :param json_data:
+        :return:
+        """
+        new_data = copy.deepcopy(json_data)
+        for page in new_data:
+            lst_additional_movies = []
+            for movie in page['results']:
+                movie = Movie(movie)
+                new_movie_element = copy.deepcopy(movie)
+                if int(movie.release_date[:4]) > 2000 and "Crime" in movie.genres:
+                    index = new_movie_element.genres.index("Crime")
+                    new_movie_element.genres[index] = "New_Crime"
+                    lst_additional_movies.append(new_movie_element)
+                elif int(movie.release_date[:4]) < 2000 and "Drama" in movie.genres:
+                    index = new_movie_element.genres.index("Drama")
+                    new_movie_element.genres[index] = "Old_Drama"
+                    lst_additional_movies.append(new_movie_element)
+                elif int(movie.release_date[:4]) == 2000:
+                    new_movie_element.genres.append("New_Century")
+                    lst_additional_movies.append(new_movie_element)
+            page['results'].extend(lst_additional_movies)
+        return new_data
     # ----------------Task BODY---------------------------------------
     with open("movies.json", "r") as json_data:
-        data = pickle.loads(json_data)
-        print(data)
+        data = json.load(json_data)
+
+    new_data = seek_create_modify(data) # Creates new data file by adding new movies as Movie class objects to 'result'
+    writable_json_data = cus_encoder(new_data) # Translates newly added Movie class objects into dictionaries
+    with open("movies.json", "w") as json_file:
+        json.dump(writable_json_data,json_file,indent=2)
 Task01.write_function(task1_body)
-
-
-def task2_body():
-    pass
-    # ----------------Task Variables----------------------------------
-    # ----------------Task Classes------------------------------------
-    # ----------------Task Functions----------------------------------
-    # ----------------Task BODY---------------------------------------
-Task02.write_function(task2_body)
-
-
-def task3_body():
-    pass
-    # ----------------Task Variables----------------------------------
-    # ----------------Task Classes------------------------------------
-    # ----------------Task Functions----------------------------------
-    # ----------------Task BODY---------------------------------------
-Task03.write_function(task3_body)
 
 # -------------MAIN CODE---------------------------------------------------------------------
 
