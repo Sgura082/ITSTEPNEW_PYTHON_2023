dd72bbb2e110cf39d597ad56878eef459f1077e7
Saba gurashvili <Sgura082@gmail.com>
2024-05-06 22:28:52 +0400

    finished with psycopg2

--- Lesson33/Homework33.py
+++ Lesson33/Homework33.py
@@ -1,5 +1,4 @@
 import psycopg2
-
 HOST = 'localhost'
 PORT = 5432
 DATABASE = 'Lesson33'
@@ -9,38 +8,59 @@ PASSWORD = 'XX132Files'
 conn = psycopg2.connect(host=HOST,port=PORT,database=DATABASE,user=USER,password=PASSWORD)
 cursor = conn.cursor()
 
-Query_list =[]
+Query_Send_list =[]
+Query_Fetch_list = []
 #----Query for Department table creation---------------------------------------------------------------
 create_Department_table_query = """
-create table Department(DepartmentID varchar(10), DepartmentName varchar(30))
+create table Department(DepartmentID varchar(10) unique, DepartmentName varchar(30))
 """
-Query_list.append(create_Department_table_query)
+Query_Send_list.append(create_Department_table_query)
 
 
 #----Query for Employee table creation---------------------------------------------------------------
 create_Employee_table_query = """
-create table Employee(EmployeeID varchar(10), Fullname varchar(30),HireDate timestamp, DepartmentID varchar(10)
+create table Employee(EmployeeID varchar(10), Fullname varchar(30),HireDate Timestamp, DepartmentID varchar(10))
 """
-Query_list.append(create_Employee_table_query)
+Query_Send_list.append(create_Employee_table_query)
 
 #----Query for altering DepartmentID in Employee table so that it becomes a foreign key---------------------------------
 alter_Employee_table_query = """
-alter table Employee add constraint DepartmentIDconst foreign key (DepartmentID) references Departments (DepartmentID)
+alter table Employee add constraint DepartmentIDconst foreign key (DepartmentID) references Department (DepartmentID)
 """
-Query_list.append(alter_Employee_table_query)
+Query_Send_list.append(alter_Employee_table_query)
 
 #----Query for Data input into Department table---------------------------------------------------------------
-# insert_query = """
-#     insert into customers(customerid, surname, firstname, city) values (
-#         '10000011','Frank', 'Sinatra','ny')"""
-# cursor.execute(insert_query )
-# conn.commit()
-# cursor.close()
-# conn.close()
+insert_Departments_query = """
+    insert into Department (DepartmentID, DepartmentName) values ('10001','Management'),('10002','IT'),('10003','Sales')
+    """
+Query_Send_list.append(insert_Departments_query)
+
+#----Query for Data input into Employee table---------------------------------------------------------------
+insert_Employee_query = """
+    insert into Employee(EmployeeID, Fullname ,HireDate , DepartmentID) values ('30001','Saba Gurashvili','16/12/2023','10002'),
+    ('30002','George Gergianson','01/01/2021','10001'),('30003','Makhatma Ghlonti','01/04/1991','10001'),
+    ('30004','Karl Marx','10/10/1851','10003')
+    """
+Query_Send_list.append(insert_Employee_query)
+
+#----Query for Fetching all data---------------------------------------------------------------
+Fetch_joined_data_query = """
+    select * from employee
+    left join department on department.departmentID = Employee.Departmentid
+    """
+Query_Fetch_list.append(Fetch_joined_data_query)
 
 #----Query Sender---------------------------------------------------------------
-for query in Query_list:
+for query in Query_Send_list:
     cursor.execute(query)
     conn.commit()
-    cursor.close()
-    conn.close()
+cursor.close()
+conn.close()
+
+#----Query Fetcher--------------------------------------------------------------
+for query in Query_Fetch_list:
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+cursor.close()
